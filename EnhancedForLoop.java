912b23690e24bd477c57ac7e90cdb295abb5fc52
Erik Blom <eblom2629@greenvilleschools.us>
2018-01-30 10:13:45 -0500

    finished the problems

--- EnhancedForLoop.java
+++ EnhancedForLoop.java
@@ -56,41 +56,81 @@ public class EnhancedForLoop
         //      for loop to return the mean of the elements in nums. Test
         //      the method here.
         
+        double[] nums = {5,6,7,9,15,40,3};
         
         
-
+        double sum = 0;
+        for(double x: nums)
+        {
+            sum += x;
+        }
+        System.out.println(sum/(nums.length));
+        
         // #2 - Write a method max(double nums[]) which uses an enhanced
         //      for loop to return the maximum value of the elements in
         //      nums. Then test the method.
-
-
-
+        
+        double max = nums[0];
+        for(double x: nums)
+        {
+            if(x > max) max = x;
+            
+        }
+        System.out.println(max);
         
         // #3 - Write a method sumEvenVals that uses an enhanced for 
         //      loop to return the sum of all even values stored in an 
         //      Array.  Then test the method.
-
-
-
+        
+        double sumEven = 0;
+        for(double x: nums)
+        {
+            if(x%2 == 0) sumEven += x;
+        }
+        System.out.println(sumEven);
 
         // #4 - Write a method vowelCount that uses an enhanced for loop
         //      to count the number of vowels (a,e,o,u only) in a list
         //      of words.
         
+        String[] words = {"dogs", "cats", "tree", "letter", "apple"};
         
-        
+        int vowelCount = 0;
+        for(String word: words)
+        {
+            for(int i = 0; i < word.length(); i++){
+                char letter = word.charAt(i);
+                if(letter == 'a' || letter == 'i' || letter == 'e' || letter == 'o' || letter == 'u'){
+                    vowelCount ++;
+                }
+            }
+        }
+        System.out.println(vowelCount);
 
         // #5 - Write a method sumEvenLocs that uses an enhanced for 
         //      loop to find the sum of all values stored with even 
         //      numbered indices in an Array. Then test the method.
         
-        
-        
+        int sumEvenLocs1 = 0;
+        int index = 0; 
+        for(double x: nums)
+        {
+            if(index%2 == 0) sumEvenLocs1 += x;
+            index ++;
+        }
+        System.out.println(sumEvenLocs1);
         
         // #6 - Solve #5 using a regular for loop. Then explain why a
         //      regular for loop is more appropriate for that problem.
         
+        int sumEvenLocs2 = 0; 
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i%2 == 0) sumEvenLocs2 += nums[i]; 
+        }
+        System.out.println(sumEvenLocs2);
         
-        
+        // a regular for loop makes more sense because it references and tracks the index of the loop
+        // the advanced loop doesnt keep track of the loop
     }
 }
